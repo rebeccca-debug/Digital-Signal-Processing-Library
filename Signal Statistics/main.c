fa2d4f1575425b17cef9cbee8cae4804e4f3881f
Rebecca Shillingford <54553333+rebeccca-debug@users.noreply.github.com>
2022-01-19 18:36:01 -0500

    Update main.c

--- Signal Statistics/main.c
+++ Signal Statistics/main.c
@@ -13,6 +13,9 @@ double MEAN;
 double VARIANCE;
 double STD;
 
+/** @brief: Signal statistical algorithms in C. Calculate the mean, variance, and standard deviation of the ECG signals.
+*
+*/
 int main(){
 
     MEAN = calc_signal_mean(&Input_Signal_f32_1kHz_15kHz[0], SIG_LENGTH);
@@ -25,6 +28,10 @@ int main(){
     return 0;
 }
 
+/** @brief: Calculate the mean of the signal. Algorithm: μ = 1/N * ^(N-1)Σ_(i=0)x_i
+* @param sig_src_arr double pointer to signal array
+* @param sig_lenth integer representing length of the signal
+*/
 double calc_signal_mean(double *sig_src_arr, int sig_length){
     double _mean = 0.0;
     for (int i = 0; i < sig_length; i++){
@@ -35,6 +42,11 @@ double calc_signal_mean(double *sig_src_arr, int sig_length){
     return _mean;
 }
 
+/** @brief: Calculate the mean of the signal (how far 'i' deviates from the mean). Algorithm: 1/(N-1) * σ = ^1/(N-1)Σ_(i=0)(x_i-μ)^2
+* @param sig_src_arr double pointer to signal array
+* @param sig_mean double representing the signal mean
+* @param sig_length integer representing length of the signal
+*/
 double calc_signal_variance(double *sig_src_arr, double sig_mean, int sig_length){
     double _variance = 0.0;
     for (int i = 0; i < sig_length; i++){
@@ -45,6 +57,9 @@ double calc_signal_variance(double *sig_src_arr, double sig_mean, int sig_length
     return _variance;
 }
 
+/** @brief: Calculate the mean of the signal. Algorithm: σ^2 = (^1/(N-1)Σ_(i=0)((x_i-μ)^2))^2
+* @param sig_variance double representing the variance of the signal
+*/
 double calc_signal_std(double sig_variance){
     double _std = sqrt(sig_variance);
     return _std;
